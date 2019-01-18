f4d3919ec8f407dcee8f57d60f4b62ec51134f2f
bhumip214 <bhumip214@gmail.com>
2019-01-18 12:28:51 -0800

    Converted Tab component with styled-component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,20 +2,57 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const TabWrapper = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const TabActive = styled(TabWrapper)`
+  background-color: #fff;
+  color: #333;
+  border: 2px solid #333;
+`;
+
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
   return (
-    <div
-      className={props.tab === props.selectedTab ? "tab active-tab" : "tab"}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+    <div>
+      {props.tab === props.selectedTab ? (
+        <TabWrapper
+          onClick={() => {
+            /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-        props.selectTabHandler(props.tab);
-      }}
-    >
-      {props.tab.toUpperCase()}
+            props.selectTabHandler(props.tab);
+          }}
+        >
+          {props.tab.toUpperCase()}
+        </TabWrapper>
+      ) : (
+        <TabActive
+          onClick={() => {
+            props.selectTabHandler(props.tab);
+          }}
+        >
+          {props.tab.toUpperCase()}
+        </TabActive>
+      )}
     </div>
   );
 };
